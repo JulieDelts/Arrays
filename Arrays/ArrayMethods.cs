3af2d70d6e410140c57e6985ec4563011551a18b
JulieDelts <juliadelcova@gmail.com>
2024-03-23 04:04:17 +0300

    Add project files.

---
+++ Arrays/ArrayMethods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public static class ArrayMethods
+    {
+        public static double GetMaxScore(double[] scores)
+        {
+            double maxScore = 0;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] > maxScore) maxScore = scores[i];
+            }
+            return maxScore;
+        }
+
+        public static double[] CalculatePricesWithMarkup(double[] prices, double markup)
+        {
+            double[] pricesWithMarkup = new double[prices.Length];
+            for (int i = 0; i < prices.Length; i++)
+            {
+                pricesWithMarkup[i] = prices[i] + prices[i] * markup/100; 
+            }
+            return pricesWithMarkup;
+        }
+
+        public static int CountnUnderachievingClerks(double[] results, double requiredSum)
+        {
+            int count = 0;
+            for(int i = 0; i < results.Length; i++) 
+            {
+                if (results[i] < requiredSum) count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using Arrays;
+
+//использование методов 
+double[] scores = new double[10] { 1, 0.2, 3, 2.3, 1.5, 0.7, 3.1, 2.7, 1.1, 0.5};
+double maxScore = ArrayMethods.GetMaxScore(scores);
+Console.WriteLine($"Максимальный рейтинг - {maxScore}");
+
+double[] prices = new double[10] { 25.5, 30, 12.2, 60, 47, 167, 150.8, 85, 134, 90.8};
+double[] pricesWithMarkup = ArrayMethods.CalculatePricesWithMarkup(prices, 18);
+Console.WriteLine("Обновленные цены:");
+for (int i = 0; i < pricesWithMarkup.Length; i++)
+{
+    Console.Write($"{pricesWithMarkup[i]:0.00} ");
+}
+Console.WriteLine();
+
+double[] results = new double[7] {1234, 2345, 3245, 2456, 1576, 1000, 2200};
+int numberOfUnderachievingClerks = ArrayMethods.CountnUnderachievingClerks(results, 2000);
+Console.WriteLine($"Количество сотрудников, не выполнивших план - {numberOfUnderachievingClerks}");
